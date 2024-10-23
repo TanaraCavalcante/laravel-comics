a522170c0930da8d52dd81d2fab361e7b64b44f1
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-10-23 19:32:46 -0400

    Cards comics

--- resources/views/pages/comics.blade.php
+++ resources/views/pages/comics.blade.php
@@ -4,15 +4,17 @@
 
 @section("main-content")
     <div class="container">
-        @foreach ($comics as $comic)
+        <div class="row justify-content-center">
+            @foreach ($comics as $comic)
 
-        <div class="card " style=" width: 18rem;">
-            <img src="{{$comic["thumb"] }}" class="card-img-top" alt="{{ $comic["title"]}}">
-            <div class="card-body">
-            <h5 class="card-title">{{ $comic["title"]}}</h5>
+            <div class="card col-3 m-2">
+                <img src="{{$comic["thumb"] }}" class="card-img-top" alt="{{ $comic["title"]}}">
+                <div class="card-body">
+                <h5 class="card-title">{{ $comic["title"]}}</h5>
+                </div>
             </div>
+            @endforeach
         </div>
-        @endforeach
     </div>
 
 
