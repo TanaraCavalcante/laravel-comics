db39859c533f445485594f45171869c3d35a9563
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-10-23 18:33:08 -0400

    resources.images

--- resources/views/pages/comics.blade.php
+++ resources/views/pages/comics.blade.php
@@ -1,13 +1,19 @@
 @extends("layouts.app")
 
-@dump($comics)
-
 @section("page-title", "comics")
 
 @section("main-content")
 
-<h1>
-    comics main
-</h1>
+    @foreach ($comics as $comic)
+        <ul>
+            {{-- <li>
+                <img src="" alt="">
+            </li> --}}
+            <li>
+                Title: {{ $comic["title"]}}
+            </li>
+        </ul>
+    @endforeach
+
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.home');
-});
 
-Route::get('/comics', function () {
+
+Route::get('/', function () {
     $comics = config("db.comics");
     return view('pages.comics', compact("comics"));
-});
+ });//->name("comics"); "posso renomear assim!"
