82973fed7bdab060b7efa09a8e5b604dfece7c34
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-10-23 20:00:24 -0400

    started header

--- resources/views/pages/comics.blade.php
+++ resources/views/pages/comics.blade.php
@@ -6,11 +6,10 @@
     <div class="container">
         <div class="row justify-content-center">
             @foreach ($comics as $comic)
-
             <div class="card col-3 m-2">
                 <img src="{{$comic["thumb"] }}" class="card-img-top" alt="{{ $comic["title"]}}">
                 <div class="card-body">
-                <h5 class="card-title">{{ $comic["title"]}}</h5>
+                <h5 class="card-title text-center">{{ $comic["title"]}}</h5>
                 </div>
             </div>
             @endforeach
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,28 @@
 
 <header>
-    <h1>
-        Header
-    </h1>
+    <nav class="navbar navbar-expand-lg  container">
+        <div class="container-fluid">
+
+            <img src="" alt="Bootstrap" width="30" height="24">
+
+            <div>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">CHARACTERS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">COMICS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="#"></a>
+                    </li>
+                </ul>
+            </div>
+
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            </form>
+
+        </div>
+      </nav>
 </header>
